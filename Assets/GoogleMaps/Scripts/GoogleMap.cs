46181f054e188633f1beeec8e742dcf254b8cced
Matko Zurak <matkezicro@gmail.com>
2015-04-28 18:32:57 +0200

    nothing important

---
+++ Assets/GoogleMaps/Scripts/GoogleMap.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+using System.Collections;
+
+public class GoogleMap : MonoBehaviour
+{
+	public enum MapType
+	{
+		RoadMap,
+		Satellite,
+		Terrain,
+		Hybrid
+	}
+	public bool loadOnStart = true;
+	public bool autoLocateCenter = true;
+	public GoogleMapLocation centerLocation;
+	public int zoom = 13;
+	public MapType mapType;
+	public int size = 512;
+	public bool doubleResolution = false;
+	public GoogleMapMarker[] markers;
+	public GoogleMapPath[] paths;
+	private TerrainData tData;
+	
+	void Start() {
+		if(loadOnStart) Refresh();	
+	}
+	
+	public void Refresh() {
+		if(autoLocateCenter && (markers.Length == 0 && paths.Length == 0)) {
+			Debug.LogError("Auto Center will only work if paths or markers are used.");	
+		}
+		StartCoroutine(_Refresh());
+	}
+	
+	IEnumerator _Refresh ()
+	{
+		var url = "http://maps.googleapis.com/maps/api/staticmap";
+		var qs = "";
+		if (!autoLocateCenter) {
+			if (centerLocation.address != "")
+				qs += "center=" + WWW.UnEscapeURL (centerLocation.address);
+			else {
+				qs += "center=" + WWW.UnEscapeURL (string.Format ("{0},{1}", centerLocation.latitude, centerLocation.longitude));
+			}
+		
+			qs += "&zoom=" + zoom.ToString ();
+		}
+		qs += "&size=" + WWW.UnEscapeURL (string.Format ("{0}x{0}", size));
+		qs += "&scale=" + (doubleResolution ? "2" : "1");
+		qs += "&maptype=" + mapType.ToString ().ToLower ();
+		var usingSensor = false;
+#if UNITY_IPHONE
+		usingSensor = Input.location.isEnabledByUser && Input.location.status == LocationServiceStatus.Running;
+#endif
+		qs += "&sensor=" + (usingSensor ? "true" : "false");
+		
+		foreach (var i in markers) {
+			qs += "&markers=" + string.Format ("size:{0}|color:{1}|label:{2}", i.size.ToString ().ToLower (), i.color, i.label);
+			foreach (var loc in i.locations) {
+				if (loc.address != "")
+					qs += "|" + WWW.UnEscapeURL (loc.address);
+				else
+					qs += "|" + WWW.UnEscapeURL (string.Format ("{0},{1}", loc.latitude, loc.longitude));
+			}
+		}
+		
+		foreach (var i in paths) {
+			qs += "&path=" + string.Format ("weight:{0}|color:{1}", i.weight, i.color);
+			if(i.fill) qs += "|fillcolor:" + i.fillColor;
+			foreach (var loc in i.locations) {
+				if (loc.address != "")
+					qs += "|" + WWW.UnEscapeURL (loc.address);
+				else
+					qs += "|" + WWW.UnEscapeURL (string.Format ("{0},{1}", loc.latitude, loc.longitude));
+			}
+		}
+		
+		
+//		var req = new HTTP.Request ("GET", url + "?" + qs, true);
+//		req.Send ();
+//		while (!req.isDone)
+//			yield return null;
+//		if (req.exception == null) {
+//			var tex = new Texture2D (size, size);
+//			tex.LoadImage (req.response.Bytes);
+//			renderer.material.mainTexture = tex;
+//		}
+
+		var req = new WWW (url + "?" + qs);
+		Debug.LogError (qs);
+		yield return req;
+//		Renderer rend = GetComponent<Renderer> ();
+//		rend.enabled = true;
+//		rend.material.mainTexture = req.texture;
+
+		tData = Terrain.activeTerrain.terrainData;
+		
+		SplatPrototype[] terrainTexture = new SplatPrototype[1]; 
+		terrainTexture[0] = new SplatPrototype(); 
+		terrainTexture [0].texture = req.texture;
+		tData.splatPrototypes = terrainTexture;  
+
+	}
+	
+	
+}
+
+public enum GoogleMapColor
+{
+	black,
+	brown,
+	green,
+	purple,
+	yellow,
+	blue,
+	gray,
+	orange,
+	red,
+	white
+}
+
+[System.Serializable]
+public class GoogleMapLocation
+{
+	public string address;
+	public float latitude;
+	public float longitude;
+}
+
+[System.Serializable]
+public class GoogleMapMarker
+{
+	public enum GoogleMapMarkerSize
+	{
+		Tiny,
+		Small,
+		Mid
+	}
+	public GoogleMapMarkerSize size;
+	public GoogleMapColor color;
+	public string label;
+	public GoogleMapLocation[] locations;
+	
+}
+
+[System.Serializable]
+public class GoogleMapPath
+{
+	public int weight = 5;
+	public GoogleMapColor color;
+	public bool fill = false;
+	public GoogleMapColor fillColor;
+	public GoogleMapLocation[] locations;	
+}
---
+++ Assets/Scripts/CamZoomJS.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+//zoom camera
+var distance : float = 60;
+var sensitivityDistance : float = 50;
+var damping : float = 5;
+var minFOV : float = 40;
+var maxFOV : float = 60;
+ 
+function Start () {
+        distance = GetComponent.<Camera>().fieldOfView;
+}
+ 
+function Update () {
+ 
+        distance -= Input.GetAxis("Mouse ScrollWheel") * sensitivityDistance;
+        distance = Mathf.Clamp(distance, minFOV, maxFOV);
+        GetComponent.<Camera>().fieldOfView = Mathf.Lerp(GetComponent.<Camera>().fieldOfView, distance, Time.deltaTime * damping);
+}
